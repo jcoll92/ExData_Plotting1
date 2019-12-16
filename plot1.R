6b0140dacefea596d99d67c6b7ef03148b785337
jcoll92 <john.colley@veritec.com.au>
2019-12-16 14:38:14 +1100

    Changes include:
    - turning global active power to numeric
    - code to create png file.

--- plot1.R
+++ plot1.R
@@ -2,10 +2,13 @@
 
 housedata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 data <- subset(housedata,Date %in% c("1/2/2007", "2/2/2007"))
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
 
 # The data.frame titled 'data' dimensions are 2880 observations of 9 variables. 
 
 
 # Plot1.png is created with the following code:
 
+png(file = "filelocation/plot1.png", width = 480, height = 480)
 hist(data$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+dev.off()
