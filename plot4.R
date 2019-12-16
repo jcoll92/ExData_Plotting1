1f06cfa01cf8e8e12d6834d128cbb207ef01ffdd
jcoll92 <john.colley@veritec.com.au>
2019-12-16 15:47:46 +1100

    Committing plot code and png for plot4

---
+++ plot4.R
@@ -0,0 +1,33 @@
+# Assuming the 'household_power_consumption.txt' file is in your workig directory, the following steps can be followed to read in the data.
+
+readnames <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 6)
+
+householddata <- read.table("household_power_consumption.txt", header=TRUE,sep=";", skip=66630, 
+                            nrows=2900, col.names=names(readnames), na.strings=c("?"),
+                            colClasses=c("character", "character","numeric","numeric","numeric","numeric",
+                                         "numeric","numeric","numeric"))
+
+householddata$Date<-as.Date(householddata$Date, format = "%d/%m/%Y")
+householddata$Time<-strptime(paste(householddata$Date,householddata$Time),"%F %T")
+
+hhdata <- subset(householddata,householddata$Date %in% as.Date(c("2007-02-01", "2007-02-02")))
+
+# The data.frame titled 'hhdata' dimensions are 2880 observations of 9 variables. 
+
+
+# Plot4.png is created with the following code:
+
+png(file = "filelocation.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+# Plot 4.1
+plot(hhdata$Time, hhdata$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "", pch = ".", type = "l")
+# Plot 4.2
+plot(hhdata$Time, hhdata$Voltage, xlab = "datetime", ylab = "Voltage", pch = ".", type = "l")
+# Plot 4.3
+plot(hhdata$Time, hhdata$Sub_metering_1, ylab = "Energy sub metering", xlab = "", pch = ".", type = "l", col = "black")
+points(hhdata$Time, hhdata$Sub_metering_2, col = "red", type = "l")
+points(hhdata$Time, hhdata$Sub_metering_3, col = "blue", type = "l")
+legend("topright", lwd = 1, col = c("black", "red", "blue"), legend = names(hhdata[,7:9]))
+# Plot 4.4
+plot(hhdata$Time, hhdata$Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power", pch = ".", type = "l")
+dev.off()
